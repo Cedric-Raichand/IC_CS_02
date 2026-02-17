321c901f70a95e941656d471a2b97c82448f4005
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2026-02-17 01:40:56 -0930

    Define User and File models with RSA key fields and encrypted file storage

--- models.py
+++ models.py
@@ -3,11 +3,18 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(300), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    public_key = db.Column(db.Text, nullable=False)
+    private_key = db.Column(db.Text, nullable=False)
 
-    public_key = db.Column(db.Text, nullable=True)
-    private_key = db.Column(db.Text, nullable=True)
 
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(200))
+    stored_filename = db.Column(db.String(200))
+    encrypted_key = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
