eff28c0763f2a801eb8bf0645de6253ab1e4fc2a
DZODZODZI CEDRICK <cdzodzodzi@st.ug.edu.gh>
2026-02-17 01:44:49 -0930

    Add RSA key generation and AES key encryption/decryption functions

--- rsa_utils.py
+++ rsa_utils.py
@@ -1,21 +1,5 @@
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
-import base64
 
 def generate_keys():
     key = RSA.generate(2048)
-    private_key = key.export_key().decode()
-    public_key = key.publickey().export_key().decode()
-    return public_key, private_key
-
-def encrypt_key(aes_key, public_key_str):
-    public_key = RSA.import_key(public_key_str.encode())
-    cipher = PKCS1_OAEP.new(public_key)
-    encrypted = cipher.encrypt(aes_key)
-    return base64.b64encode(encrypted).decode()
-
-def decrypt_key(encrypted_key, private_key_str):
-    private_key = RSA.import_key(private_key_str.encode())
-    cipher = PKCS1_OAEP.new(private_key)
-    decrypted = cipher.decrypt(base64.b64decode(encrypted_key))
-    return decrypted
+    return key.publickey().export_key().decode(), key.export_key().decode()
